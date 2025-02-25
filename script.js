254aca90025a4e516acc7d693d0db3a2ab2c519a
lukasmaag <lu12maag@gmail.com>
2025-02-25 19:36:19 +0100

    first commit

---
+++ script.js
@@ -0,0 +1,46 @@
+
+function add(a, b) {
+  const result = a + b;
+  return result;
+}
+const number1 = add(58, 22);
+const number2 = add(94, 21);
+
+console.log(number1);
+console.log(number2);
+
+function subtract(a, b) {
+  const result = a - b;
+  return result;
+}
+const number3 = subtract(58, 22);
+const number4 = subtract(94, 21);
+
+console.log(number3);
+console.log(number4);
+
+function multiply(a, b) {
+  const result = a * b;
+  return result;
+}
+const number5 = multiply(58, 22);
+const number6 = multiply(94, 21);
+
+console.log(number5);
+console.log(number6);
+
+function divide(a, b) {
+  const result = a / b;
+  return result;
+}
+const number7 = divide(58, 22);
+const number8 = divide(94, 21);
+
+console.log(number7);
+console.log(number8);
+
+
+let vorname = "Hans";
+let nachname = "Wurst";
+let vollerName = vorname + " " + nachname;
+console.log(vollerName);
