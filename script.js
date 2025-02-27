0bf4c8dce73dfc50e18f9992cce1c3c0e1f2e1ac
lukasmaag <lu12maag@gmail.com>
2025-02-27 19:11:00 +0100

    Functions and Objects

--- script.js
+++ script.js
@@ -1,4 +1,3 @@
-
 function add(a, b) {
   const result = a + b;
   return result;
@@ -39,13 +38,10 @@ const number8 = divide(94, 21);
 console.log(number7);
 console.log(number8);
 
-
 let vorname = "Hans";
 let nachname = "Wurst";
 let vollerName = vorname + " " + nachname;
 console.log(vollerName);
-
-
 
 // Arrays
 
@@ -57,7 +53,80 @@ cars.sort();
 
 console.log(cars.join(" | "));
 
-cars.forEach(car => {
+cars.forEach((car) => {
   console.log(car);
 });
 
+// Modify Array
+function modifyArray(firstElement, lastElement) {
+  cars.unshift(firstElement);
+  cars.push(lastElement);
+  return cars;
+}
+console.log("Original Array:", cars);
+modifyArray("Tesla", "Ferrari");
+console.log("Modified Array:", cars);
+
+// Find Car
+function findCar(carName) {
+  if (cars.includes(carName)) {
+    console.log(`Gefunden: ${carName}`);
+  } else {
+    console.log("Nicht gefunden");
+  }
+}
+findCar("Toyota");
+findCar("Fiat");
+findCar("BMW");
+
+// Car Stats
+function getCarStats() {
+  let totalLength = 0;
+  let carsOverFive = 0;
+  let longestCar = cars[0];
+
+  cars.forEach((car) => {
+    totalLength += car.length;
+
+    if (car.length > 5) {
+      carsOverFive++;
+    }
+
+    if (car.length > longestCar.length) {
+      longestCar = car;
+    }
+  });
+
+  const averageLength = totalLength / cars.length;
+
+  console.log(
+    `Durchschnittliche Länge: ${averageLength.toFixed(2)} Buchstaben`
+  );
+  console.log(`Autos mit mehr als 5 Buchstaben: ${carsOverFive}`);
+  console.log(`Auto mit längstem Namen: ${longestCar}`);
+}
+getCarStats();
+
+// Convert to Upper Case
+function convertToUpperCase(carName) {
+  const upperCaseName = carName.toUpperCase();
+  console.log(upperCaseName);
+  return upperCaseName;
+}
+
+const result = convertToUpperCase("Porsche");
+
+// Objects
+const car = {
+  brand: "Toyota",
+  model: "Corolla",
+  year: 2020,
+};
+
+function printCarInfo(car) {
+  console.log(
+    `Das Auto ist ein ${car.brand} ${car.model} aus dem Jahr ${car.year}.`
+  );
+}
+
+printCarInfo(car);
